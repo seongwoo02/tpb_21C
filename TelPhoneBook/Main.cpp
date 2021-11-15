780ae42a6690670bf3ea46f7b3b923d98e1449d2
chaeseongwoo <seongu658@naver.com>
2021-11-15 09:41:22 +0900

    2. 기본 메뉴인 Util.cpp 파일에 Menu() 함수를 추가하고 Main.cpp에서 사용하도록 함.    Github에서 사용하는 README.md 파일을 생성함.

--- TelPhoneBook/Main.cpp
+++ TelPhoneBook/Main.cpp
@@ -1,21 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "Util.cpp"
 
-void Menu() {
-	puts("==========================");
-	puts("1. 전화번호 등록");
-	puts("2. 전화번호 출력");
-	puts("3. 전화번호 검색");
-	puts("4. 전화번호 삭제");
-	puts("5. 전화번호 Text 파일 출력");
-	puts("0. 프로그램 종료");
-	puts("");
-}
 int main() {
 	int run = 1;
 	while (run) {
-		puts("메뉴를 선택하시오.");
+		Menu();
+		puts("메뉴를 선택하시오 : ");
 		char c = getchar(); getchar();
 		switch (c)
 		{
